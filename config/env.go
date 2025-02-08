3a9029dd5813ac11062f6ce8b87b13c1f2c31749
Sean Burman <sb9443353@gmail.com>
2025-02-08 14:19:44 -0700

    Updated config vars

--- config/env.go
+++ config/env.go
@@ -10,14 +10,12 @@ import (
 
 // Environment variables
 type Vars struct {
-	HOST                 string
-	PORT                 string
-	MONGO_URI            string
-	SECRET               string
-	CLIENT_ID            string
-	CLIENT_SECRET        string
-	GOOGLE_CLIENT_ID     string
-	GOOGLE_CLIENT_SECRET string
+	HOST          string
+	PORT          string
+	MONGO_URI     string
+	SECRET        string
+	CLIENT_ID     string
+	CLIENT_SECRET string
 }
 
 // Env() returns Vars struct of environment variables
@@ -26,18 +24,16 @@ func Env() Vars {
 	if flag.Lookup("test.v") == nil {
 		err := godotenv.Load()
 		if err != nil {
-			log.Fatal("Error loading environment variables")
+			log.Println("Error loading environment variables")
 		}
 	}
 
 	return Vars{
-		HOST:                 os.Getenv("HOST"),
-		PORT:                 os.Getenv("PORT"),
-		MONGO_URI:            os.Getenv("MONGO_URI"),
-		SECRET:               os.Getenv("SECRET"),
-		CLIENT_ID:            os.Getenv("CLIENT_ID"),
-		CLIENT_SECRET:        os.Getenv("CLIENT_SECRET"),
-		GOOGLE_CLIENT_ID:     os.Getenv("GOOGLE_CLIENT_ID"),
-		GOOGLE_CLIENT_SECRET: os.Getenv("GOOGLE_CLIENT_SECRET"),
+		HOST:          os.Getenv("HOST"),
+		PORT:          os.Getenv("PORT"),
+		MONGO_URI:     os.Getenv("MONGO_URI"),
+		SECRET:        os.Getenv("SECRET"),
+		CLIENT_ID:     os.Getenv("CLIENT_ID"),
+		CLIENT_SECRET: os.Getenv("CLIENT_SECRET"),
 	}
 }
