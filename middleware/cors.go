26d9896e76e0ac526d6448045335dd3cb64f5f3f
Sean Burman <sb9443353@gmail.com>
2025-02-13 20:54:38 -0700

    cors

--- middleware/cors.go
+++ middleware/cors.go
@@ -8,8 +8,8 @@ import (
 
 func MiddlewareCORS(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		fmt.Println("Request from: ", c.Request().Host)
-		c.Response().Header().Set("Access-Control-Allow-Origin", "https://sb-api-sever-3-afb294a915a2.herokuapp.com")
+		fmt.Println("Request from: ", c.Request())
+		c.Response().Header().Set("Access-Control-Allow-Origin", "https://bitscrawler.com, https://www.bitscrawler.com")
 		c.Response().Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH")
 		c.Response().Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, Client_id, Client_secret")
 		c.Response().Header().Set("Access-Control-Allow-Credentials", "true")
