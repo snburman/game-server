96bf93263c7cf872d3930d26a996a9fb4110c277
Sean Burman <sb9443353@gmail.com>
2025-02-13 20:47:00 -0700

    cors

--- middleware/cors.go
+++ middleware/cors.go
@@ -8,7 +8,7 @@ import (
 
 func MiddlewareCORS(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		fmt.Println("Request from: ", c.Request().RemoteAddr)
+		fmt.Println("Request from: ", c.Request().Host)
 		c.Response().Header().Set("Access-Control-Allow-Origin", "*")
 		c.Response().Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH")
 		c.Response().Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, Client_id, Client_secret")
