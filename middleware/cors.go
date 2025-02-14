3ba090bcf904cc38b0290e0f51b1c3608842c895
Sean Burman <sb9443353@gmail.com>
2025-02-13 20:44:54 -0700

    cors

--- middleware/cors.go
+++ middleware/cors.go
@@ -1,9 +1,14 @@
 package middleware
 
-import "github.com/labstack/echo/v4"
+import (
+	"fmt"
+
+	"github.com/labstack/echo/v4"
+)
 
 func MiddlewareCORS(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
+		fmt.Println("Request from: ", c.Request().RemoteAddr)
 		c.Response().Header().Set("Access-Control-Allow-Origin", "*")
 		c.Response().Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH")
 		c.Response().Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, Client_id, Client_secret")
